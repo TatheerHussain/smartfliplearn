5ed435802b0c21a4cf7d5a851f01d50cf5100a3c
Tatheer Hussain Mir <tatheerhussain@outlook.com>
2019-01-28 17:55:28 +0800

    app support page

---
+++ app_support/index.js
@@ -0,0 +1,8 @@
+/* jshint node: true */
+"use strict";
+
+module.exports = function(app) {
+  app.get('/support', function(req, res){
+    res.sendFile(__dirname + '/views/support.html');
+  });
+};
