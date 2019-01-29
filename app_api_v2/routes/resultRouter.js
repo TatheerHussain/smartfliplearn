67984c73abe50cde0fb4b4950c16232efde68841
Tatheer Hussain Mir <tatheerhussain@outlook.com>
2019-01-29 15:11:56 +0800

    result routerupdate

---
+++ app_api_v2/routes/resultRouter.js
@@ -0,0 +1,43 @@
+/* jshint node: true */
+/* jshint esversion: 6 */
+
+
+"use strict";
+
+var express = require('express');
+var resultRouter = express.Router();
+
+var lectureController = require('./../controllers/lectureController');
+var tokenController = require('./../controllers/tokenController');
+
+/**
+Get Instructor Results
+
+GET	/api_v2/result/{lecture_id}/
+
+Authentication:   user token
+Authorization:    instructor
+
+Path Parameters:  lecture_id
+Query String:     none
+Request Body: 	  none
+**/
+resultRouter.route('/:LECTUREID/')
+    .get(lectureController.getInstructorResults);
+
+/**
+Get Student Results
+
+GET	/api_v2/result/{lecture_id}/student/{student_id}/
+
+Authentication:   user token
+Authorization:    instructor
+
+Path Parameters:  lecture_id String	required
+Query String:     none
+Request Body: 	  none
+**/
+resultRouter.route('/:LECTUREID/student/:STUDENTID')
+    .get(lectureController.getStudentResults);
+
+module.exports = resultRouter;
